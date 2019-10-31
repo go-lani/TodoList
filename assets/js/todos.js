6a674aa38c4678a4fe945ed55643abae9a0e31a9
publizm <publizm@gmail.com>
2019-10-31 18:29:51 +0900

    refactor: refactory function to prevent blank values from being entered

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,8 +1,10 @@
-const $input  = document.querySelector('#add-input');
+const $input = document.querySelector('#add-input');
 const $addBtn = document.querySelector('#add-btn');
-const $ul     = document.querySelector('#todos');
+const $ul = document.querySelector('#todos');
 
 $addBtn.addEventListener('click', function () {
+  if (!$input.value) return alert('해야할 일을 입력해주세요.');
+
   $ul.innerHTML += `<li class="todos">${$input.value}</li>`;
   $input.value = '';
   $input.focus();
