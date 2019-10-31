aba1a477d397b6a89f4dc9cbc42ee24d502bd723
publizm <publizm@gmail.com>
2019-10-31 18:49:31 +0900

    docs: create input keyup event function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -5,7 +5,18 @@ const $ul = document.querySelector('#todos');
 $addBtn.addEventListener('click', function () {
   if (!$input.value) return alert('해야할 일을 입력해주세요.');
 
-  $ul.innerHTML += `<li class="todos">${$input.value}</li>`;
+  $ul.innerHTML += `<li class="todos"><label><input type="checkbox">${$input.value}</label></li>`;
   $input.value = '';
   $input.focus();
+});
+
+$input.addEventListener('keyup', function (e) {
+  if (!(e.keyCode === 13)) return;
+
+  if (!$input.value) return alert('해야할 일을 입력해주세요.');
+
+  $ul.innerHTML += `<li class="todos"><label><input type="checkbox">${$input.value}</label></li>`;
+  $input.value = '';
+  $input.focus();
+
 });
