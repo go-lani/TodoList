4ba55d499baf9d439e775afdcde74644b90b1d12
publizm <publizm@gmail.com>
2019-10-31 17:18:54 +0900

    docs: add todo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,2 +1,9 @@
-(function () {
-}());
+const $input  = document.querySelector('#add-input');
+const $addBtn = document.querySelector('#add-btn');
+const $ul     = document.querySelector('#todos');
+
+$addBtn.addEventListener('click', function () {
+  $ul.innerHTML += `<li class="todos">${$input.value}</li>`;
+  $input.value = '';
+  $input.focus();
+});
