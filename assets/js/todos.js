cb17f61527cebdab16337697125593739ce6d3c0
publizm <publizm@gmail.com>
2019-10-31 18:57:35 +0900

    refactor: Separation to create todo list function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,21 +2,20 @@ const $input = document.querySelector('#add-input');
 const $addBtn = document.querySelector('#add-btn');
 const $ul = document.querySelector('#todos');
 
-$addBtn.addEventListener('click', function () {
+function addTodo(value) {
   if (!$input.value) return alert('해야할 일을 입력해주세요.');
 
-  $ul.innerHTML += `<li class="todos"><label><input type="checkbox">${$input.value}</label></li>`;
+  $ul.innerHTML += `<li class="todos"><label><input type="checkbox">${value}</label><button type="button">삭제</button></button></li>`;
   $input.value = '';
   $input.focus();
+}
+
+$addBtn.addEventListener('click', function () {
+  addTodo($input.value);
 });
 
 $input.addEventListener('keyup', function (e) {
   if (!(e.keyCode === 13)) return;
 
-  if (!$input.value) return alert('해야할 일을 입력해주세요.');
-
-  $ul.innerHTML += `<li class="todos"><label><input type="checkbox">${$input.value}</label></li>`;
-  $input.value = '';
-  $input.focus();
-
+  addTodo($input.value);
 });
